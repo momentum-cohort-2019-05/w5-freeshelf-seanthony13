3030b48771772433125e640d625751b2de8f0fb0
seanthony13 <seanthony13@gmail.com>
2019-06-30 21:46:25 -0400

    some things work

--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from book import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
 
 # Use include() to add paths from the catalog application 
